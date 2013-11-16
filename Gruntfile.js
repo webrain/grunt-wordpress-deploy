810887b76ad8e79374c66c5d942a5a1b452d699d
Dario Ghilardi <darioghilardi@gmail.com>
2013-11-16 18:25:48 +0100

    Cleaned Gruntfile.js.

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,51 +10,41 @@
 
 module.exports = function(grunt) {
 
-  // Project configuration.
   grunt.initConfig({
-    db_fixture: grunt.file.readJSON('test/fixtures/test_db.json'),
-
     jshint: {
       all: [
         'Gruntfile.js',
         'tasks/*.js',
-        '<%= nodeunit.tests %>',
       ],
       options: {
         jshintrc: '.jshintrc',
       },
     },
 
-    // Before generating any new files, remove any previously-created files.
     clean: {
       tests: ['tmp'],
     },
 
-    deployments: {
-        options: {
-            backups_dir: ''
-        },
-        local: '<%= db_fixture.local %>',
-        develop: '<%= db_fixture.develop %>'
+    wordpressdeploy: {
+      options: {
+        backups_dir: 'backups_dir/'
+      }
     },
 
-    // Unit tests.
     nodeunit: {
-      tests: ['test/*_test.js'],
+      tasks: ['test/*_test.js']
     },
   });
 
-  // Actually load this plugin's task(s).
   grunt.loadTasks('tasks');
 
-  // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
   // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'deployments', 'nodeunit']);
+  grunt.registerTask('test', ['clean', 'nodeunit']);
 
   // By default, lint and run all tests.
   grunt.registerTask('default', ['jshint', 'test']);
